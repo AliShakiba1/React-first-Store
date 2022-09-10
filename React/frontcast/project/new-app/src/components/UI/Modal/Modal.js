e179a541e3b14cad660b3d1c89b6e927a942aba0
Ali <prance5731.mania@gmail.com>
2022-09-10 13:15:23 +0430

    add backDrop - start of 69

--- React/frontcast/project/new-app/src/components/UI/Modal/Modal.js
+++ React/frontcast/project/new-app/src/components/UI/Modal/Modal.js
@@ -1,17 +1,22 @@
 import React from 'react'
+import Wrapper from '../../../hoc/Wrapper'
+import BackDrop from '../BackDrop/BackDrop'
 import './Modal.css'
 
 const Modal = props => {
   return (
-    <div
-      className="modal"
-      style={{
-        transform: props.showOrNot ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.showOrNot ? '1' : '0',
-      }}
-    >
-      {props.children}
-    </div>
+    <Wrapper>
+      <BackDrop show={props.showOrNot} click={props.modalClose} />
+      <div
+        className="modal"
+        style={{
+          transform: props.showOrNot ? 'translateY(0)' : 'translateY(-100vh)',
+          opacity: props.showOrNot ? '1' : '0',
+        }}
+      >
+        {props.children}
+      </div>
+    </Wrapper>
   )
 }
 
