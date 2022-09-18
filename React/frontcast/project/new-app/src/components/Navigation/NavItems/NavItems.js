fbc3f1f6cc5c58ed1c7e1f2fe26bee8b4246964f
AliShakiba <prance5731.mania@gmail.com>
2022-09-18 13:08:50 +0430

    adding acount page

--- React/frontcast/project/new-app/src/components/Navigation/NavItems/NavItems.js
+++ React/frontcast/project/new-app/src/components/Navigation/NavItems/NavItems.js
@@ -6,7 +6,12 @@ const NavItems = props => {
   return (
     <ul className="Nav-items">
       <NavItem link="/">Shopping</NavItem>
+<<<<<<< HEAD
       <NavItem link="/">Checkout</NavItem>
+=======
+      <NavItem link="/checkout">Checkout</NavItem>
+      <NavItem link="/account">Account</NavItem>
+>>>>>>> 3cf3cec... adding acount page
     </ul>
   )
 }
---
+++ React/frontcast/project/new-app/src/containers/Account/Account.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+class Account extends React.Component {
+  render() {
+    return <h2>Account page</h2>
+  }
+}
+export default Account
